74848cc85dec9861fd2ca510ee078b681d72ecd6
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-07-04 08:25:55 +0530

    Add files via upload

---
+++ Project1/Myapp1/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def mor_view(request):
+    msg = '<h1> Good Morning </h1>'
+    return HttpResponse(msg)
+
+def aft_view(request):
+    msg = '<h1> Good Afternoon </h1>'
+    return HttpResponse(msg)
+
+def eve_view(request):
+    msg = '<h1> Good Evening</h1>'
+    return HttpResponse(msg)
+
+def nig_view(request):
+    msg = '<h1> Good Night </h1>'
+    return HttpResponse(msg)
