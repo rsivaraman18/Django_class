e891ce4ea306366db8bef5a97b32a9ad46ee52dd
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-08-16 08:42:16 +0530

    Add files via upload

---
+++ Project6_SQCRUD2/project6_SQCRUD2/urls.py
@@ -0,0 +1,24 @@
+
+from django.contrib import admin
+from django.urls import path
+from Upload_app import views
+
+''' Medis path kuduka itha import pandran'''
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register',views.Lets_Register),
+    path('',views.Lets_View,name="dis_view"),
+    path('update/<int:cid>',views.Lets_update),
+    path('delete/<int:cid>',views.Lets_delete),
+    
+]
+
+
+
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
