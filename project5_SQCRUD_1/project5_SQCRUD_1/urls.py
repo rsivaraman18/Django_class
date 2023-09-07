43d089d4bdc1f50f854e71d2118ac76bc4bc29e0
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-09-07 19:42:55 +0530

    Update urls.py

--- project5_SQCRUD_1/project5_SQCRUD_1/urls.py
+++ project5_SQCRUD_1/project5_SQCRUD_1/urls.py
@@ -1,6 +1,6 @@
 """ 
 URL configuration for project5_SQCRUD_1 project.
-   
+    
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
